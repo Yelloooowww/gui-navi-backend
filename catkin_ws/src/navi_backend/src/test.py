6daae6c4b6b9719e1806ce6b3053018fd640115a
yelloooowww <cococing0324@gmail.com>
2021-02-02 07:02:54 +0800

    okkkkkkkkk

--- catkin_ws/src/navi_backend/src/test.py
+++ catkin_ws/src/navi_backend/src/test.py
@@ -1,26 +1,12 @@
 import json
 import requests
 
-for test_time in range(0,50):
-    import requests
+url = "http://robotx.cowbon.info/anchorwaypoints/274"
+payload = 'mid=1&globalx=-9&globaly=0.0&uwbid=1'
+headers = {
+  'Content-Type': 'application/x-www-form-urlencoded'
+}
 
-    url = "http://robotx.cowbon.info/anchorwaypoints/"+str(test_time)
-    payload = {}
-    headers= {}
-    response = requests.request("GET", url, headers=headers, data = payload)
-    tmp = response.json()
-    if not 'aid' in tmp.keys() : continue
-    print(tmp)
+response = requests.request("PUT", url, headers=headers, data = payload)
 
-# import requests
-#
-# url = "http://robotx.cowbon.info/anchorwaypoints/34"
-#
-# payload = 'mid=1&globalx=12.345678901&globaly=9.0&uwbid=1'
-# headers = {
-#   'Content-Type': 'application/x-www-form-urlencoded'
-# }
-#
-# response = requests.request("PUT", url, headers=headers, data = payload)
-#
-# print(response.text.encode('utf8'))
+print(response.text.encode('utf8'))
--- catkin_ws/src/navi_backend/src/ui_backend.py
+++ catkin_ws/src/navi_backend/src/ui_backend.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
 import numpy as np
 import pandas as pd
 import rospy
@@ -65,20 +66,6 @@ class AnchorWaypoint:
 		self.uwbid = tmp['uwbid']
 
 
-
-# class VehState:
-# 	def __init__(self):
-# 		self.vsid = None
-# 		self.mid = None
-# 		self.vid = None
-# 		self.timestamp = None
-# 		self.powerlevel = None
-# 		self.tempcpu = None
-# 		self.tempenv = None
-# 		self.globalx = None #DECIMAL(11,9)
-# 		self.globaly = None #DECIMAL(12,9)
-
-
 def rotate(degree, x, y):
   theta = np.radians(degree)
   c, s = np.cos(theta), np.sin(theta)
@@ -90,46 +77,47 @@ def rotate(degree, x, y):
 class BH_Cave:
 	def __init__(self):
 		self.anchor_list = []
+
+		self.fig, self.ax = plt.subplots()
+		self.scat = None
+		self.text = None
+		self.goal_scat = None
+
+		for i in range(1,999):
+			if not rospy.has_param("/loc_a"+str(i)+"/aid"):continue
+			x,y = rospy.get_param("/loc_a"+str(i)+"/globalx"),rospy.get_param("/loc_a"+str(i)+"/globaly")
+			anc = AnchorWaypoint(mid=1,globalx=x,globaly=y,isanchor=True,uwbid=0)
+			self.anchor_list.append(anc)
+
+
+	def update_plot(self, frame):
+		if not self.scat == None:self.scat.remove()
+		if not self.text == None:self.text.remove()
+		for item in self.anchor_list:
+			item.get()
+			a_x, a_y = rotate(32, float(item.globalx),float(item.globaly))
+			self.scat = plt.scatter(a_x,a_y-1, s=90, c = 'r', marker='^', zorder=3)
+			self.text = plt.text(a_x-1, a_y, str(item.aid), fontsize=10, c = 'r')
+
+
+	def plot_init(self):
 		while not rospy.has_param("ui_backend/pkg_path") and not rospy.is_shutdown():
 			rospy.loginfo("Wait for ui_backend/pkg_path")
 			rospy.sleep(0.5)
 		map_path = rospy.get_param("ui_backend/pkg_path") + rospy.get_param("/map")
-
-		fig, ax = plt.subplots()
 		im = plt.imread(map_path)
-
-		pt_bl = (-73, -19.25) # bottom left corner in meters
-		pt_tr = (2, 16) # top left corner in meters
-		scale_x = im.shape[0] / (pt_tr[1] - pt_bl[1] + 1) # pixels / meter
-		scale_y = im.shape[1] / (pt_tr[0] - pt_bl[0] + 1) # pixels / meter
-
-		ax.imshow(im, aspect=1, extent=(-73, 2, -19.25, 16))
-		for i in range(1,999):
-			if not rospy.has_param("/loc_a"+str(i)+"/aid"):continue
-
-			x,y = rospy.get_param("/loc_a"+str(i)+"/globalx"),rospy.get_param("/loc_a"+str(i)+"/globaly")
-			# a_x, a_y = rotate(32, x,y)
-			# plt.scatter(a_x,a_y-1, s=150, c = 'r', marker='^', zorder=3)
-			# plt.text(a_x-2, a_y, str(i), fontsize=14, c = 'r')
-			anc = AnchorWaypoint(mid=1,globalx=x,globaly=y,isanchor=True,uwbid=0)
-			self.anchor_list.append(anc)
-
-		cid = fig.canvas.mpl_connect('button_press_event', self.onclick)
-		self.timer = rospy.Timer(rospy.Duration(0.2), self.update)
-
-	def update(self,event):
-		for item in self.anchor_list:
-			item.get()
-			a_x, a_y = rotate(32, float(item.globalx),float(item.globaly))
-			plt.scatter(a_x,a_y-1, s=150, c = 'r', marker='^', zorder=3)
-			plt.text(a_x-2, a_y, str(item.aid), fontsize=14, c = 'r')
+		self.ax.imshow(im, aspect=1, extent=(-73, 2, -19.25, 16))
+		cid = self.fig.canvas.mpl_connect('button_press_event', self.onclick)
 
 	def onclick(self,event):
 		if event.dblclick:
-			print(event.x, event.y, event.xdata, event.ydata)
+			# print(event.x, event.y, event.xdata, event.ydata)
+			if not self.goal_scat==None :self.goal_scat.remove()
+			self.goal_scat = plt.scatter(event.xdata,event.ydata, s=70, c = 'b', marker='o', zorder=3)
 
 
 if __name__ == '__main__':
 	rospy.init_node("navi_backend")
-	BH_Cave()
+	bh_cave = BH_Cave()
+	ani = FuncAnimation(bh_cave.fig, bh_cave.update_plot, init_func=bh_cave.plot_init)
 	plt.show(block=True)
